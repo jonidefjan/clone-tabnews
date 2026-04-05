34da631b6eb40ab8bb0e83dcacbab37356fb1a5a
jonidefjan <jonidefjan@gmail.com>
2026-04-05 03:07:05 -0300

    feat: move content inside body tag for proper HTML structure

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,10 @@ function Home() {
           name="google-site-verification"
           content="E5p9xmCE70SuymR_rw1qLOptxSMUmPfM5RekYtGL8_I"
         />
-        <h1>Obrigado por acreditar em mim!</h1>
       </Head>
+      <body>
+        <h1>Obrigado por acreditar em mim!</h1>
+      </body>
     </>
   );
 }
